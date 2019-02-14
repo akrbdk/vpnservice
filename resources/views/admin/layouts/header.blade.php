08ea59b70aceeee7359e0605a8c6fa69025de191
Sergey Kubaichuk <Strange9852@list.ru>
2019-02-14 04:32:44 +0300

    /admin trial hide

--- resources/views/admin/layouts/header.blade.php
+++ resources/views/admin/layouts/header.blade.php
@@ -29,9 +29,11 @@
                 <li>
                     <a href="{{ url('/download') }}">{{ trans('menu.download') }}</a>
                 </li>
+                @guest
                 <li class="free-test">
                     <a href="{{ url('/plans') }}">{{ trans('menu.testIt') }}</a>
                 </li>
+                @endguest
             </ul>
         </nav>
     </div>
