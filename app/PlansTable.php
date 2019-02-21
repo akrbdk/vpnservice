af6c915adce5546f3ee836dc52e21a6e72d9acdc
Alex <IceAlexxx@gmail.com>
2019-02-21 21:55:23 +0300

    Fix

--- app/PlansTable.php
+++ app/PlansTable.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 use DB;
 use Auth;
 
-class PlansTable
+class PlansTable extends Model
 {
     /**
      * Таблица, связанная с моделью.
@@ -14,20 +14,6 @@ class PlansTable
      * @var string
      */
     protected $table = 'card_view';
-    private $plans;
-    private $cards;
-
-    public function __construct() {
-      $plans = DB::table('plans_table')->select('id','price','months_limit')->orderBy('id')->get();
-      $cards = DB::table('card_view')->get();
-      for
-    }
-
-    public function getPrice($plandId) {
-      foreach ($this->plans as $plan) {
-        if ($plan->id === (int)$plandId) return $plan->price;
-      }
-    }
 
     public static function isHidden(){
       $data = '';
