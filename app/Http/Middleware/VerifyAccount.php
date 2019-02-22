8ed6f8da84de0400cd177e57f336c73c50e6af1a
Alex <IceAlexxx@gmail.com>
2019-02-22 23:44:25 +0300

    Removed APIController dependency

--- app/Http/Middleware/VerifyAccount.php
+++ app/Http/Middleware/VerifyAccount.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use App\Http\APIUtils\APIReply;
 use App\Http\APIUtils\APICode;
 use App\Http\Controllers\ApiController;
+use DB;
 use Closure;
 
 class VerifyAccount{
@@ -28,8 +29,17 @@ class VerifyAccount{
             return APIReply::err(APICode::$invArgument, 'Empty token');
         }
 
-        $user = ApiController::checkUserPlatform($token);
-        if (!$user) {
+        /*
+            SELECT users.* FROM users
+            INNER JOIN sessions ON sessions.user_id = users.id
+            WHERE sessions.token = 'test_token';
+        */
+        $user = DB::table('users')
+                ->join('sessions', 'users.id', '=', 'sessions.user_id')
+                ->where('sessions.token', '=', $token)
+                ->first();
+
+        if (empty($user)) {
             return APIReply::err(APICode::$invArgument, 'Invalid token');
         }
 
