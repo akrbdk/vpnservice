5b0a0dbb48cd25bddf8926fa2e6ebcd080448b9e
Alex <IceAlexxx@gmail.com>
2019-02-22 22:00:25 +0300

    Created lang file for payment list

--- resources/views/site/paymentlist.blade.php
+++ resources/views/site/paymentlist.blade.php
@@ -1,19 +1,19 @@
 <thead>
   <tr>
     <th>
-        Plan
+        @lang('paymentlist.plan')
     </th>
     <th>
-        Expires on
+        @lang('paymentlist.expiry_on')
     </th>
     <th>
-        Price
+        @lang('paymentlist.price')
     </th>
     <th>
-        Method
+        @lang('paymentlist.method')
     </th>
     <th>
-        Renovation
+        @lang('paymentlist.renovation')
     </th>
   </tr>
 </thead>
@@ -27,7 +27,7 @@
     <td>
         @php
         if(time() > $pay->expiry_at){
-          echo 'Expired';
+          echo trans('paymentlist.expired');
           $button = 0;
         }
         else{
@@ -45,23 +45,23 @@
     <td>
 
       @if ($pay->auto_renew === 1)
-        <button href="#" class="btn-green">Auto paid</button>
+        <button href="#" class="btn-green">@lang('paymentlist.subscribed')</button>
       @elseif ($button === 1 && $pay->method === 'PayPal')
         <form action="{{ url('/paypalsub') }}" method="post">
           @csrf
           <input type="hidden" name="plan_id" value="{{$pay->plan_id}}">
           <input type="hidden" name="pay_id" value="{{$pay->id}}">
-          <button href="#" class="btn-green">Get Autopay</button>
+          <button href="#" class="btn-green">@lang('paymentlist.start_subscription')</button>
         </form>
       @elseif ($button === 1 && $pay->method === 'Card')
         <form action="{{ url('/stripesub') }}" method="post">
           @csrf
           <input type="hidden" name="plan_id" value="{{$pay->plan_id}}">
           <input type="hidden" name="pay_id" value="{{$pay->id}}">
-          <button href="#" class="btn-green">Get Autopay</button>
+          <button href="#" class="btn-green">@lang('paymentlist.start_subscription')</button>
         </form>
       @else
-        <a href="#" class="btn-gray">Do not renew</a>
+        <a href="#" class="btn-gray">@lang('paymentlist.unsubscribed')</a>
       @endif
     </td>
   </tr>
